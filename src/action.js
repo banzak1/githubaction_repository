297fc397ca70fd9e72a5b5c10def8aa6ab019510
banzak1 <leosantanabr@gmail.com>
2021-12-21 15:10:26 -0300

    Correçao de erros

--- src/action.js
+++ src/action.js
@@ -6,12 +6,12 @@ async function run() {
 
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
-    const { contet = {} } = github;
+    const { context = {} } = github;
     const { pull_request } = context.payload;
 
     await octokit.issues.creatComment({
         ...context.rep,
-        issue_number: pullrequest.number,
+        issue_number: pull_request.number,
         body: 'Obrigado por enviar uma pull request! Nós iremos tentar avaliá-la assim que possível. '
 
     })
