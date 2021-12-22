4feb79c27c84c946db5788af447a278a71b7d797
banzak1 <leosantanabr@gmail.com>
2021-12-22 11:41:21 -0300

    test

--- src/action.js
+++ src/action.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 const core = require('@actions/core');
 const github = require('@actions/github');
 
@@ -6,10 +6,11 @@ async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
     const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
 
-    const randomPos = Math.round(Math.random() * 10);
+    const randomPos = Math.round(Math.random() * 1000);
     const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
     const response = await fetch(url);
-    const { results } = await response.json();
+    const results = await response.json();
+    console.log(results)
     const gifUrl = results[0].media[0].tinygif.url;
 
 
